f5732d90188957c99a54e6376d687780f7b82533
Евгений <info-best@bk.ru>
2024-08-30 09:16:03 +0300

    add

--- module_5_1/pythonProject/module_5_1.py
+++ module_5_1/pythonProject/module_5_1.py
@@ -7,23 +7,28 @@ class House:
         self.new_floor = new_floor
         print(f"ЖК называется {self.name}, этажей в доме {self.number_of_floors}, этаж номер {self.new_floor} ")
 
+        if  self.number_of_floors < 1:
+            print(f"В ЖК {self.name} Такого этажа не существует")
+            return
+        if self.new_floor > self.number_of_floors:
+            print(f"В ЖК {self.name} Такого этажа не существует")
+            return
+
 
         for i in range(1,self.new_floor + 1):
             print(i)
 
 
-        if self.new_floor > self.number_of_floors:
-            print(f"В ЖК {self.name} Такого этажа не существует")
-
-        if self.new_floor < 1:
-            print(f"В ЖК {self.name} Такого этажа не существует")
 
 
 
-hous = House('ЖК Горский', 18)
-hous1 = House('Домик в деревне', 2)
-hous.go_to(5)
-hous1.go_to(7)
+
+
+
+hous = House('ЖК Горский', 5)
+hous1 = House('Домик в деревне', 7)
+hous.go_to(3)
+hous1.go_to(8)
 
 
 
